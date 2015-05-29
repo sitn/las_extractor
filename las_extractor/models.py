f657215f61e936361405a21bf10bf9404cb3593a
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2015-05-29 08:12:28 +0200

    Change model class for GeoAlchemy 2

--- las_extractor/models.py
+++ las_extractor/models.py
@@ -8,16 +8,13 @@ from sqlalchemy import (
     Integer
     )
 
-from papyrus.geo_interface import GeoInterface
-from geoalchemy import GeometryColumn, Geometry
+from geoalchemy2 import Geometry
     
 Base = sqlahelper.get_base()
 DBSession = sqlahelper.get_session()
 
-class LidarTileIndex(GeoInterface, Base):
+class LidarTileIndex(Base):
     __tablename__='grid50mfull'
     __table_args__ = {'schema': 'lidar_tile_index', 'autoload': True}
     id=Column('oid', Integer, primary_key=True)
-    geom = GeometryColumn(Geometry(srid=21781))
-
-
+    geom = Column(Geometry("POLYGON", srid=21781))
